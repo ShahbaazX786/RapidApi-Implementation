205458c516a73b7e9666a0780acab1e83aabb2fe
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-06-14 17:41:13 +0530

    CL-0.3: Chuck Norris component

---
+++ RapidApi/src/app/components/chuck-norris-jokes/chuck-norris-jokes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chuck-norris-jokes',
+  templateUrl: './chuck-norris-jokes.component.html',
+  styleUrls: ['./chuck-norris-jokes.component.css']
+})
+export class ChuckNorrisJokesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
