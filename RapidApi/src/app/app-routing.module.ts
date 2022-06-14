eb98dcd982779ac00753ce0057802aa2864eac4d
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-06-14 18:41:02 +0530

    CL-0.4: Adding routing and some components

--- RapidApi/src/app/app-routing.module.ts
+++ RapidApi/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomeComponent } from './components/home/home.component';
+import { ChuckNorrisJokesComponent } from './components/chuck-norris-jokes/chuck-norris-jokes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'chuck',component:ChuckNorrisJokesComponent},
+  {path:'**',component:pnf},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
