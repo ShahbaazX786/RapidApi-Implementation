4966c992d96048f1d630051ea3ba8f1d51474eaa
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-06-14 18:41:56 +0530

    CL-0.5: Adding routing and some components

--- RapidApi/src/app/app-routing.module.ts
+++ RapidApi/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PnfComponent } from './components/pnf/pnf.component';
 import { HomeComponent } from './components/home/home.component';
 import { ChuckNorrisJokesComponent } from './components/chuck-norris-jokes/chuck-norris-jokes.component';
 import { NgModule } from '@angular/core';
@@ -7,7 +8,7 @@ const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'chuck',component:ChuckNorrisJokesComponent},
-  {path:'**',component:pnf},
+  {path:'**',component:PnfComponent},
 ];
 
 @NgModule({
--- RapidApi/src/app/app.module.ts
+++ RapidApi/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChuckNorrisJokesComponent } from './components/chuck-norris-jokes/chuck-norris-jokes.component';
+import { HomeComponent } from './components/home/home.component';
+import { PnfComponent } from './components/pnf/pnf.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChuckNorrisJokesComponent
+    ChuckNorrisJokesComponent,
+    HomeComponent,
+    PnfComponent
   ],
   imports: [
     BrowserModule,
